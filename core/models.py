73f0c35a36f7890b26fffd8d8a3605996838ec43
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2024-12-20 22:02:52 +0500

    course model, student model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# Course Model
+class CourseModel(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+# Student Model
+class StudentModel(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    age = models.IntegerField()
+    course = models.ForeignKey(CourseModel, on_delete=models.CASCADE, related_name='students')
+    
+    def __str__(self):
+        return self.name    
